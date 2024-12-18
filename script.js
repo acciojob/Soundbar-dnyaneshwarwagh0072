3f1a3e527a10bfed6a0459048482ec52a5519f68
Dnyaneshwar Yadav Wagh <85550894+dnyaneshwarwagh0072@users.noreply.github.com>
2024-12-18 15:29:26 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,39 @@
-//your JS code here. If required.
+// Array of sounds with audio links
+const sounds = [
+    { id: 'sound1', src: 'http://commondatastorage.googleapis.com/codeskulptor-demos/DDR_assets/Sevish_-__nbsp_.mp3' },
+    { id: 'sound2', src: 'http://codeskulptor-demos.commondatastorage.googleapis.com/pang/paza-moduless.mp3' },
+    { id: 'sound3', src: 'http://commondatastorage.googleapis.com/codeskulptor-demos/pyman_assets/ateapill.ogg' }
+];
+
+// Function to stop all sounds
+function stopAllSounds() {
+    sounds.forEach(sound => {
+        const audio = document.getElementById(sound.id);
+        if (audio) {
+            audio.pause();
+            audio.currentTime = 0;
+        }
+    });
+}
+
+// Create audio elements for each sound dynamically
+sounds.forEach(sound => {
+    const audio = document.createElement('audio');
+    audio.id = sound.id;
+    audio.src = sound.src;
+    document.body.appendChild(audio);
+});
+
+// Play sound on button click
+document.querySelectorAll('.btn').forEach(button => {
+    button.addEventListener('click', () => {
+        stopAllSounds();
+        const soundId = button.getAttribute('data-sound');
+        document.getElementById(soundId).play();
+    });
+});
+
+// Stop button functionality
+document.querySelector('.stop').addEventListener('click', () => {
+    stopAllSounds();
+});
